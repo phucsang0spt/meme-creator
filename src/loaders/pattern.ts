70eecb131b35381c9d9355910fb22b890fe5e194
sang <phucsang0spt@gmail.com>
2023-02-28 01:22:58 +0700

    Fix getPattern

--- src/loaders/pattern.ts
+++ src/loaders/pattern.ts
@@ -1,6 +1,6 @@
 export function getPattern(folder: string) {
   const pattern = new RegExp(
-    `^assets/${folder}/[a-zA-Z0-9|-]+.(png|jpg|jpeg|svg)$`
+    "^assets/" + folder + "/([a-zA-Z0-9|-]+)\\.(png|jpg|jpeg|svg)"
   );
   return pattern;
 }
